127ae2610dee3b16bc61dc0a9d68f04c209bc51b
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-04 22:03:50 -0600

    Usar DATABASE_URL y actualizar PORT para Render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
-
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('gestion_taller', 'taller_user', 'KBLLgXO3YKwsWeqKZNB9D2Vi1SMLKSlC', {
-  host: 'dpg-csklmbl6l47c73bljocg-a.oregon-postgres.render.com',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
