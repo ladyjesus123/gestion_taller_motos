98237fa51de9695d6f2528898b632ff228892d0b
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-05 10:47:52 -0600

    Corrección del router y protección de rutas en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Header from './components/Header'; 
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,8 +7,6 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import Clientes from './pages/Clientes'; 
 import EditarCliente from './pages/EditarCliente';
@@ -33,11 +30,9 @@ import ListaReportes from './pages/ListaReportes';
 import EditarReporte from './pages/EditarReporte';
 import DetallesReporte from './pages/DetallesReporte';
 import Notificaciones from './pages/PanelNotificaciones';
-import PaginaInformes from "./pages/PaginaInformes";
-
+import PaginaInformes from './pages/PaginaInformes';
 
 import './App.css';
-
 
 const App = () => {
   const navigate = useNavigate();
@@ -49,45 +44,40 @@ const App = () => {
       navigate('/'); // Si no hay token, redirige al login
     }
   }, [navigate]);
-  return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Header />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/clientes" element={<Clientes />} />
-          <Route path="/editar-cliente/:id" element={<EditarCliente />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/editar-usuario/:id" element={<EditarUsuario />} />
-          <Route path="motos" element={<Motos />}/>
-          <Route path="/crear-moto" element={<CrearMoto />} />
-          <Route path="/editar-moto/:id" element={<EditarMoto />} />
-          <Route path="inspeccion-recepcion" element={<InspeccionRecepcion/>}/>
-          <Route path="/inspeccion-recepcion/listar" element={<ListaInspecciones />} />
-          <Route path="/inspeccion-recepcion/detalles/:id" element={<DetalleInspeccion />} />
-          <Route path="/ordenesTrabajo" element={<ListaOrdenes />} />
-          <Route path="/ordenesTrabajo/crear" element={<CrearOrden />} />
-          <Route path="/ordenesTrabajo/detalles/:id" element={<DetallesOrden />} />
-          <Route path="/inspeccion-recepcion/editar/:id" element={<EditarInspeccion />} />
-          <Route path="/inventario" element={<ListaProductos />} />
-          <Route path="/inventario/crear" element={<CrearProducto />} />
-          <Route path="/inventario/editar/:id" element={<EditarProducto />} />
-          <Route path="/reportes_mecanico/crear" element={<CrearReporte />} />
-          <Route path="/reportes_mecanico/listar" element={<ListaReportes />} />
-          <Route path="/reportes_mecanico/editar/:id" element={<EditarReporte />} />
-          <Route path="/reportes_mecanico/detalles/:id" element={<DetallesReporte />} />
-          <Route path="/notificaciones" element={<Notificaciones />} />
-          <Route path="/informes" element={<PaginaInformes />} />
-          
 
-          
-          {/* aqui voy a poner mis rutas para los demás módulos */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/clientes" element={<Clientes />} />
+        <Route path="/editar-cliente/:id" element={<EditarCliente />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/editar-usuario/:id" element={<EditarUsuario />} />
+        <Route path="motos" element={<Motos />} />
+        <Route path="/crear-moto" element={<CrearMoto />} />
+        <Route path="/editar-moto/:id" element={<EditarMoto />} />
+        <Route path="inspeccion-recepcion" element={<InspeccionRecepcion />} />
+        <Route path="/inspeccion-recepcion/listar" element={<ListaInspecciones />} />
+        <Route path="/inspeccion-recepcion/detalles/:id" element={<DetalleInspeccion />} />
+        <Route path="/ordenesTrabajo" element={<ListaOrdenes />} />
+        <Route path="/ordenesTrabajo/crear" element={<CrearOrden />} />
+        <Route path="/ordenesTrabajo/detalles/:id" element={<DetallesOrden />} />
+        <Route path="/inspeccion-recepcion/editar/:id" element={<EditarInspeccion />} />
+        <Route path="/inventario" element={<ListaProductos />} />
+        <Route path="/inventario/crear" element={<CrearProducto />} />
+        <Route path="/inventario/editar/:id" element={<EditarProducto />} />
+        <Route path="/reportes_mecanico/crear" element={<CrearReporte />} />
+        <Route path="/reportes_mecanico/listar" element={<ListaReportes />} />
+        <Route path="/reportes_mecanico/editar/:id" element={<EditarReporte />} />
+        <Route path="/reportes_mecanico/detalles/:id" element={<DetallesReporte />} />
+        <Route path="/notificaciones" element={<Notificaciones />} />
+        <Route path="/informes" element={<PaginaInformes />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
