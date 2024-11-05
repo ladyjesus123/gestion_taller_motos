53aa79d21fd5ecbb13d9bd0178c4481ba709ef84
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-05 11:49:12 -0600

    correccion de config/datbase.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,11 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('gestion_taller', 'taller_user', 'NUEVA_CONTRASEÑA', {
+  host: 'dpg-csklmbl6l47c73bljocg-a.oregon-postgres.render.com',
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
-    },
-  },
+      rejectUnauthorized: false
+    }
+  }
 });
-
-module.exports = sequelize;
