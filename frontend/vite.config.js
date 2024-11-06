9fc5b5b23a8da58b6f689d70f856bec092bec95a
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-05 22:54:15 -0600

    modificacion del archivo vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
-  publicDir: 'public', // Carpeta donde se colocan los archivos estáticos sin transformación
+  base: './', // Base relativa para la aplicación en producción
   build: {
-    outDir: 'dist', // Carpeta donde se generarán los archivos después del build
-    assetsDir: 'assets', // Directorio dentro de 'dist' donde se colocarán los recursos estáticos como CSS, JS, imágenes, etc.
-  },
-  server: {
-    headers: {
-      'Content-Type': 'text/css', // Esta línea puede ayudar, pero Vite ya debería configurarlo automáticamente
-    },
-    port: 4000, // Opcionalmente, puedes especificar un puerto para el entorno de desarrollo
-    open: true, // Abrirá automáticamente el navegador cuando el servidor de desarrollo esté listo
+    outDir: 'dist', // Carpeta donde se generarán los archivos del build
   },
 });
