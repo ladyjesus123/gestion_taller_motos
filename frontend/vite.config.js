4ca85ac7f834c1b4efde7853bd4c07939b1305ca
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-05 20:28:58 -0600

    configuracion del vite.config y del index.html para rchivos en txt/css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/gestion_taller_motos/' : '/',
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // Asegúrate de que sea '/'
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos después del build
+    assetsDir: 'assets', // Directorio para los archivos estáticos
+  },
+  server: {
+    mimeTypes: {
+      'text/css': ['css'], // Esta línea asegura que los archivos CSS se sirvan como 'text/css'
+    },
+  },
 });
