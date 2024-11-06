3e71617aa6f25cbe06fec2c7bdd604141ed6d454
ladycastiblanco <leidyrcmjdes@gmail.com>
2024-11-05 22:59:12 -0600

    Corregir rutas de recursos y servir frontend optimizado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Base relativa para la aplicación en producción
+  base: '/', // Base relativa para la aplicación en producción
   build: {
     outDir: 'dist', // Carpeta donde se generarán los archivos del build
   },
